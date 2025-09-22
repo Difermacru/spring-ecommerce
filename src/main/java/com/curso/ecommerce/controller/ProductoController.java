e5e435871ef4ead1a0f8b4884cc01318f3305d83
diego_marino <dfmarinoc@gmail.com>
2025-09-22 12:34:32 -0300

    funcionalidad para editar productos

--- src/main/java/com/curso/ecommerce/controller/ProductoController.java
+++ src/main/java/com/curso/ecommerce/controller/ProductoController.java
@@ -1,11 +1,14 @@
 package com.curso.ecommerce.controller;
 
+import java.util.Optional;
+
 import org.slf4j.*;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -61,5 +64,41 @@ public class ProductoController {
 		//DESPUES DE GUARDAR EL Producto SE REDIRECCIONA A HACIA LA RUTA productos
 		return "redirect:/productos";
 	}
+	
+	//SE CREA EL METODO GET CON SU RUTA
+	@GetMapping("/edit/{id}")
+	//SE CREA EL METODO EDIT Y SE LE PASA COMO PAMETRO EL ID POR EL CUAL SE BUSCARA
+	public String edit(@PathVariable Integer id, Model model){
+		
+		//SE CREA UN NUEVO OBJETO DE TIPO Producto, SERA EL QUE ALMACENARA EL PRODUCTO QUE SE BUSCA POR EL ID
+		Producto producto= new Producto();
+		
+		//SE USA EL METODO get DE ProductoService PARA BUSCAR EL PRDUCTO POR EL ID 
+		Optional<Producto> optionalProducto=productoService.get(id);
+		
+		//CUANDO ENCUENTRA EL PRODUCTO SE ALMACENA EN LA VARIABLE producto
+		producto= optionalProducto.get();
+		
+		LOGGER.info("Producto buscado: {}",producto);
+		
+		//MODEL ENVIARA DATOS DESDE EL BACKEND HACIA LA VISTA
+		model.addAttribute("producto", producto);
+		
+		//RENDERIZA EL ARCHIVO edit
+		return "productos/edit";
+	}
+	
+	//SE CREA EL METODO POST CON SU RUTA
+	@PostMapping("/update")
+	//SE CREA EL METODO update SE PASA COMO PARAMETRO EL OBJETO Producto
+	public String update (Producto producto) {
+		
+		//SE LLAMA EL METODO update DEFINIDO EN EL ProductoService Y SE LE PASA EL PRODUCTO A MODIFICAR
+		productoService.update(producto);
+		
+		//Y DESPUES DE ACTUALIZAR SE REDICCIONA HACIA LA RUTA productos
+		return"redirect:/productos";
+	}
+	
 	
 }
