9c7b53ef1afdaf09fb7a6bb2425a4599d606b9bc
diego_marino <dfmarinoc@gmail.com>
2025-10-19 23:25:10 -0300

    metodo para generar el numero de la orden

--- src/main/java/com/curso/ecommerce/service/IDetalleOrdenService.java
+++ src/main/java/com/curso/ecommerce/service/IDetalleOrdenService.java
@@ -1,5 +1,7 @@
 package com.curso.ecommerce.service;
 
-public interface IDetalleOrdenService {
+import com.curso.ecommerce.model.DetalleOrden;
 
+public interface IDetalleOrdenService {
+	DetalleOrden save(DetalleOrden detalleOrden);
 }
