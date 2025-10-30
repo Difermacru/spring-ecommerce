a2b052440e17aaadd500cc5dcdd09c047b2f9cc9
diego_marino <dfmarinoc@gmail.com>
2025-10-30 00:52:13 -0300

    mostrar la vista para el user

--- src/main/java/com/curso/ecommerce/controller/UsuarioController.java
+++ src/main/java/com/curso/ecommerce/controller/UsuarioController.java
@@ -6,6 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -78,5 +79,11 @@ public class UsuarioController {
 		
 		//En cualquier caso (usuario no existe), redirige al home.
 		return "redirect:/";
+	}
+	
+	@GetMapping("/compras")
+	public String obtenerComprar(Model model, HttpSession session) {
+		model.addAttribute("session", session.getAttribute("idusuario"));
+		return "usuario/compras";
 	}
 }
